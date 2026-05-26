9049914962256b09152b1efceb7ee07ae520f1f6
Diar <diarselmani10@gmail.com>
2026-05-26 11:36:03 +0200

    login fix

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,21 +1,52 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import Login from "./pages/Login.js";
-
 import Register from "./pages/Register.js";
-
 import Homepage from "./pages/Homepage.js";
 import UserStats from "./pages/UserStats.js";
+import ProtectedRoute from "./context/protectedRoutes.js";
+import { useAuth } from "./context/useAuth.js";
+
+function PublicRoute({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth();
+  return user ? <Navigate to="/homepage" replace /> : children;
+}
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/homepage" element={<Homepage />} />
-      <Route path="/userstats" element={<UserStats />} />
-
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/homepage"
+        element={
+          <ProtectedRoute>
+            <Homepage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/userstats"
+        element={
+          <ProtectedRoute>
+            <UserStats />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
